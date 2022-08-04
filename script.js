a8d2ebb604a13f0a91bd33525ed24c48025293b2
SarahBolton <bolton.l.sarah@gmail.com>
2022-08-03 22:46:02 -0400

    nav to other pages

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const sideNav = document.querySelector(".sideNav");
+const closeBtn = document.querySelector(".closeBtn");
+
+hamburgerMenu.addEventListener('click', function (e) {
+    e.preventDefault()
+    hamburgerMenu.classList.toggle("active");
+    sideNav.classList.toggle("active");
+    console.log("hi")
+})
+
+closeBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    hamburgerMenu.classList.remove("active");
+    sideNav.classList.remove("active");
+    console.log("sup")
+})
